49ec15fa255d0717c5ad889fab21275e819c4908
tutkuince <tutku.ince@outlook.com>
2018-10-26 19:54:49 +0300

    CreateDemo for main app

---
+++ src/main/java/com/muditasoft/hibernateorm/_01onetoone/unidirectional/CreateDemo.java
@@ -0,0 +1,41 @@
+package com.muditasoft.hibernateorm._01onetoone.unidirectional;
+
+import org.hibernate.Session;
+
+import com.muditasoft.hibernateorm.config.HibernateConfig;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 26, 2018
+ */
+public class CreateDemo {
+	public static void main(String[] args) {
+
+		// Create Session
+		Session session = HibernateConfig.getSessionFactory().getCurrentSession();
+
+		try {
+			// Create the objects
+			Instructor instructor = new Instructor("Tutku", "Ince", "incetutku@gmail.com");
+
+			InstructorDetails instructorDetails = new InstructorDetails("Tutku Ince", "Coding");
+
+			// associate the objects
+			instructor.setInstructorDetails(instructorDetails);
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the instructor.
+			// NOTE : this will also save the details object because of CascadeType.ALL
+			System.out.println("Saving instructor: " + instructor);
+			session.save(instructor);
+
+			// commit transaction
+			session.getTransaction().commit();
+		} finally {
+			HibernateConfig.shutdown();
+		}
+	}
+}
