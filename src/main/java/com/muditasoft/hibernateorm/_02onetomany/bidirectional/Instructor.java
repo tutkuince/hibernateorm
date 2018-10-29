2de541289752efe0ef244433ec3793e2551e05cb
tutkuince <tutku.ince@outlook.com>
2018-10-29 14:22:02 +0300

    Instructor class

---
+++ src/main/java/com/muditasoft/hibernateorm/_02onetomany/bidirectional/Instructor.java
@@ -0,0 +1,89 @@
+package com.muditasoft.hibernateorm._02onetomany.bidirectional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 29, 2018
+ */
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "surname")
+	private String surname;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_details_id")
+	private InstructorDetails instructorDetails;
+
+	public Instructor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Instructor(String name, String surname, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetails getInstructorDetails() {
+		return instructorDetails;
+	}
+
+	public void setInstructorDetails(InstructorDetails instructorDetails) {
+		this.instructorDetails = instructorDetails;
+	}
+}
