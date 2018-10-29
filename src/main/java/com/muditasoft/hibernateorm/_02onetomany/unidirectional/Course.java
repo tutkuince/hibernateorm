634d3e73fc05b75b5969fd1373ad7db552e7f372
tutkuince <tutku.ince@outlook.com>
2018-10-29 16:33:47 +0300

    Course

---
+++ src/main/java/com/muditasoft/hibernateorm/_02onetomany/unidirectional/Course.java
@@ -0,0 +1,70 @@
+package com.muditasoft.hibernateorm._02onetomany.unidirectional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 29, 2018
+ */
+@Entity
+@Table(name = "course")
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title", length = 128)
+	private String title;
+
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinColumn(name = "instructor_id")
+	private Instructor instructor;
+
+	public Course() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Course(String title) {
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + "]";
+	}
+
+}
