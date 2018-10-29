3f39933a10ffb8c114e655c92c08418e1b92f72d
tutkuince <tutku.ince@outlook.com>
2018-10-29 14:22:21 +0300

    InstructorDetails class

---
+++ src/main/java/com/muditasoft/hibernateorm/_02onetomany/bidirectional/InstructorDetails.java
@@ -0,0 +1,77 @@
+package com.muditasoft.hibernateorm._02onetomany.bidirectional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 29, 2018
+ */
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "youtube_channel")
+	private String youtubeChannel;
+
+	@Column(name = "hobby")
+	private String hobby;
+
+	@OneToOne(mappedBy = "instructorDetails", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH }) // Bi-directional
+	private Instructor instructor;
+
+	public InstructorDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InstructorDetails(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+}
