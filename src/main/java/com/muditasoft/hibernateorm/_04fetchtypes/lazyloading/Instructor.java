e5e0fe1af3f5d8dbe7398988c7a94e2a97ff20bd
tutkuince <tutku.ince@outlook.com>
2018-10-29 15:49:30 +0300

    Instructor

---
+++ src/main/java/com/muditasoft/hibernateorm/_04fetchtypes/lazyloading/Instructor.java
@@ -0,0 +1,120 @@
+package com.muditasoft.hibernateorm._04fetchtypes.lazyloading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 29, 2018
+ */
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "surname")
+	private String surname;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_details_id")
+	private InstructorDetails instructorDetails;
+
+	@OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE,
+			CascadeType.PERSIST, CascadeType.REFRESH })
+	private List<Course> courses;
+
+	public Instructor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Instructor(String name, String surname, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetails getInstructorDetails() {
+		return instructorDetails;
+	}
+
+	public void setInstructorDetails(InstructorDetails instructorDetails) {
+		this.instructorDetails = instructorDetails;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email + "]";
+	}
+
+	public void addCourse(Course course) {
+		if (courses == null)
+			courses = new ArrayList<>();
+
+		courses.add(course);
+		course.setInstructor(this);
+	}
+
+}
