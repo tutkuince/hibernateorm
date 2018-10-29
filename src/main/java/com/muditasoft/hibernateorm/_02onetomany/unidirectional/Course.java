ac5400985c1901c31a4ebabbbacb4f9c23dc3703
tutkuince <tutku.ince@outlook.com>
2018-10-29 16:57:52 +0300

    added getter and setter methods for getReviews field

--- src/main/java/com/muditasoft/hibernateorm/_02onetomany/unidirectional/Course.java
+++ src/main/java/com/muditasoft/hibernateorm/_02onetomany/unidirectional/Course.java
@@ -34,8 +34,7 @@ public class Course {
 	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
-	
-	
+
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
@@ -72,15 +71,23 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
 	}
-	
+
 	public void addReview(Review review) {
-		if(reviews == null)
+		if (reviews == null)
 			reviews = new ArrayList<>();
-		
+
 		reviews.add(review);
 	}
 
