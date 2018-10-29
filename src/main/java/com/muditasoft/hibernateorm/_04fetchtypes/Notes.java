9429cd28818e11b4ad33469b501948fef9a347a8
tutkuince <tutku.ince@outlook.com>
2018-10-29 15:57:14 +0300

    Notes about Fetch Types

---
+++ src/main/java/com/muditasoft/hibernateorm/_04fetchtypes/Notes.java
@@ -0,0 +1,19 @@
+package com.muditasoft.hibernateorm._04fetchtypes;
+
+/**
+ * @author tutku ince
+ *
+ * Oct 29, 2018
+ */
+public class Notes {
+
+	/**
+	 * ------------ | ---------------
+	 * Mapping		| Default Fetch Type
+	 * ------------ | ---------------
+	 * @OneToOne	| FetchType.EAGER
+	 * @OneToMany	| FetchType.LAZY
+	 * @ManyToOne	| FetchType.EAGER
+	 * @ManyToMany	| FetchType.LAZY
+	 */
+}
