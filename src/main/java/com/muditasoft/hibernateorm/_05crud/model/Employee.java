bcf563711c32f9eb5d31473765cd5af002b5e764
tutkuince <tutku.ince@outlook.com>
2018-10-30 15:53:53 +0300

    Employee class

---
+++ src/main/java/com/muditasoft/hibernateorm/_05crud/model/Employee.java
@@ -0,0 +1,68 @@
+package com.muditasoft.hibernateorm._05crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author tutku ince
+ *
+ *         Oct 30, 2018
+ */
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 55)
+	private String name;
+
+	@Column(length = 55)
+	private String surname;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", surname=" + surname + "]";
+	}
+
+}
